49decfbc2f2b45a148b936965fa2df6bfdec0eed
kuanglingxi <kuanglingxi@bytedance.com>
2021-04-10 09:34:31 +0800

    init: 初始化 demo

---
+++ component/ClassDemo.jsx
@@ -0,0 +1,20 @@
+import LXReact from 'lxreact';
+
+export class ClassDemo extends LXReact.LXComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: 'class demo data'
+    }
+  }
+  render() {
+    return (
+      <div style={{border: '1px solid black'}}>
+        <div>class demo</div>
+        <div>{this.state.data}</div>
+        <button onClick={() => this.setState({data: Math.random()})} >class demo setState</button>
+        {this.props.children}
+      </div>
+    )
+  }
+}
